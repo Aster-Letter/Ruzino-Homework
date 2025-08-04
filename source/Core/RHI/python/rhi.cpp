13be07a4fc751ebd823ba3591c97e45baa6f0c80
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2025-08-04 20:16:14 +0800

    WIP

--- source/Core/RHI/python/rhi.cpp
+++ source/Core/RHI/python/rhi.cpp
@@ -1,19 +1,19 @@
-#include <nanobind/nanobind.h>
-
-#include <RHI/rhi.hpp>
-
-namespace nb = nanobind;
-
-using namespace USTC_CG;
-
-NB_MODULE(RHI_py, m)
-{
-    m.def(
-        "init",
-        &RHI::init,
-        nb::arg("with_window") = false,
-        nb::arg("use_dx12") = false);
-    m.def("shutdown", &RHI::shutdown);
-    m.def("get_device", &RHI::get_device, nb::rv_policy::reference);
-    m.def("get_backend", &RHI::get_backend);
-}
+//#include <nanobind/nanobind.h>
+//
+//#include <RHI/rhi.hpp>
+//
+//namespace nb = nanobind;
+//
+//using namespace USTC_CG;
+//
+//NB_MODULE(RHI_py, m)
+//{
+//    m.def(
+//        "init",
+//        &RHI::init,
+//        nb::arg("with_window") = false,
+//        nb::arg("use_dx12") = false);
+//    m.def("shutdown", &RHI::shutdown);
+//    m.def("get_device", &RHI::get_device, nb::rv_policy::reference);
+//    m.def("get_backend", &RHI::get_backend);
+//}
